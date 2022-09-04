bd2bd79f3f3a0d8c24d457557391f5b8632fc01f
Frederino <failfred98@gmail.com>
2022-09-04 16:29:11 +0800

    Add reset password and delete account functions

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,5 +1,6 @@
 import { auth } from "../config/firebase-config.js"
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, sendPasswordResetEmail } from "firebase/auth";
+import admin from "../config/firebase-service.js";
 
 export const createUserAccount = async (req, res) => {
 	try {
@@ -12,7 +13,24 @@ export const createUserAccount = async (req, res) => {
 			})
 		});
 	} catch (error) {
-		console.log(error.message);
+		return res.status(400).json({
+			message: error.message
+		})
+	}
+}
+
+export const deleteUser = async (req, res) => {
+	try {
+		const { user } = req.body;
+		await admin.auth().deleteUser(user.uid).then(() => {
+			return res.status(200).json({
+				message: "user deleted!"
+			})
+		})
+	} catch (error) {
+		return res.status(400).json({
+			message: error.message
+		})
 	}
 }
 
@@ -22,23 +40,43 @@ export const loginUser = async (req, res) => {
 		await signInWithEmailAndPassword(auth, email, password).then((userCred) => {
 			return res.status(200).json({
 				user: userCred.user,
-				token: userCred.user.stsTokenManager,
+				accessToken: userCred.user.stsTokenManager.accessToken,
+				refreshToken: userCred.user.stsTokenManager.refreshToken,
 				message: "user logged in!"
 			})
 		});
 	} catch (error) {
-		console.log(error.message);
+		return res.status(400).json({
+			message: error.message
+		})
 	}
 }
 
 export const logoutUser = async (req, res) => {
 	try {
-		await signOut(auth).then((userCred) => {
+		await signOut(auth).then(() => {
 			return res.status(200).json({
 				message: "user logged out!"
 			})
 		})
 	} catch (error) {
-		console.log(error.message);
+		return res.status(400).json({
+			message: error.message
+		})
+	}
+}
+
+export const resetPassword = async (req, res) => {
+	try {
+		const { email } = req.body;
+		await sendPasswordResetEmail(auth, email).then(() => {
+			return res.status(200).json({
+				message: "email sent!"
+			})
+		})
+	} catch (error) {
+		return res.status(400).json({
+			message: error.message
+		})
 	}
 }
--- user-service/index.js
+++ user-service/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { checkIfAuthenticated } from './middleware/firebase-middleware.js';
-import { createUserAccount, loginUser, logoutUser } from './controller/user-controller.js';
+import { createUserAccount, deleteUser, loginUser, logoutUser, resetPassword } from './controller/user-controller.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
@@ -13,14 +13,16 @@ const router = express.Router()
 
 // Controller will contain all the User-defined Routes
 router.get('/', (_, res) => res.send('Hello World from user-service'));
-router.get('/firebaseauth/user', checkIfAuthenticated, (_, res) => res.send("true"));
+router.get('/firebaseauth/authenticate', checkIfAuthenticated, (_, res) => res.send("true"));
 router.post('/firebaseauth/login', loginUser);
 router.post('/firebaseauth/signup', createUserAccount);
 router.post('/firebaseauth/logout', logoutUser);
+router.post('/firebaseauth/deleteuser', deleteUser);
+router.post('/firebaseauth/resetpassword', resetPassword);
 
 app.use('/api', router).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
 app.listen(8000, () => console.log('user-service listening on port 8000'));
