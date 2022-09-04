b1f64892b578d22dacc1790c5996f94a1b7bbae7
Ong Kim Lai <kimlailfc@gmail.com>
2022-09-04 14:04:38 +0800

    Added popover and logout button

--- frontend/src/pages/user-service/NavBar.js
+++ frontend/src/pages/user-service/NavBar.js
@@ -1,5 +1,5 @@
-import { HStack, Heading, Avatar, useColorMode } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react"
+import { HStack, Heading, Avatar, useColorMode, Button, Popover, 
+    PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 function NavBar() {
@@ -9,12 +9,21 @@ function NavBar() {
     <HStack w="100%" px="3%" py="1%" justifyContent="space-between">
         <Heading color="#66ddaa">PeerPrep</Heading>
         <HStack>
-            <Button 
-            variant='link'
-            onClick={() => toggleColorMode()}>
+            <Button variant='link' onClick={() => toggleColorMode()}>
                 {colorMode === "dark" ? <SunIcon color='orange.200'/> : <MoonIcon color='blue.800'/> }
             </Button>
-        <Avatar name="Sasuke Uchiha" src="https://bit.ly/broken-link" />
+            <Popover>
+                <PopoverTrigger>
+                    <Avatar name="Sasuke Uchiha" src="https://bit.ly/broken-link" click />
+                </PopoverTrigger>
+                <PopoverContent width='30'>
+                    <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverBody>
+                            <Button colorScheme='teal'>Logout</Button>
+                        </PopoverBody>
+                </PopoverContent>
+            </Popover>
         </HStack>
     </HStack>
     );
