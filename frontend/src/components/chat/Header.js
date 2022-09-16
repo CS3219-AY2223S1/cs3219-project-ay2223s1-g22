3745c3855b14050b44e8c4dda8ffd3c6c5922a41
James Yeap <jamesyeap.ys@gmail.com>
2022-09-16 19:46:15 +0800

    Add username to chat box header

--- frontend/src/components/chat/Header.js
+++ frontend/src/components/chat/Header.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Flex, Avatar, Text } from "@chakra-ui/react";
 
+import UserContext from "../../UserContext";
+
 const Header = () => {
+  const { user } = useContext(UserContext);
+
+  /* ======== Helper Functions ================================================================================*/
+  function getUsernameFromEmail(user) {
+    if (!user || !user.email) {
+      return "NO USERNAME FOUND";
+    }
+
+    const email = user.email;
+    const username = email.split("@")[0];
+
+    return username;
+  }
+
   return (
     <Flex w="100%">
-      <Avatar marginLeft='1' marginTop='1' size="sm" />
+      <Avatar marginLeft="1" marginTop="1" size="sm" />
       <Flex flexDirection="column" mx="3" justify="center">
         <Text fontSize="sm" fontWeight="bold">
-          <p>{"<Insert Name Here>"}</p>
+          <p>{getUsernameFromEmail(user)}</p>
         </Text>
       </Flex>
     </Flex>
