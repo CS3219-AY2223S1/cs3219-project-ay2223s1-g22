c0b315c6e921793d4c8339e7df0584aa0b861b53
Frederino <failfred98@gmail.com>
2022-09-17 02:25:03 +0800

    Add delete account button

--- frontend/src/pages/user-service/NavBar.js
+++ frontend/src/pages/user-service/NavBar.js
@@ -1,5 +1,6 @@
 import {
 	HStack,
+	Stack,
 	Heading,
 	Avatar,
 	useColorMode,
@@ -10,20 +11,51 @@ import {
 	PopoverArrow,
 	PopoverCloseButton,
 	PopoverBody,
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	useDisclosure,
+	useToast
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import UserContext from "../../UserContext";
 import { useNavigate, useLocation } from "react-router-dom";
 import LeaveRoomOverlay from "../matching-service/LeaveRoomOverlay";
+import { deleteUserAccount } from "../../controller/user-controller";
 
 function NavBar() {
-	const location = useLocation()
+	const location = useLocation();
 	const { colorMode, toggleColorMode } = useColorMode();
-
 	const { idToken, user, clearUserData } = useContext(UserContext);
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
+	const cancelRef = useRef();
 	const navigate = useNavigate();
-	const [isOpen, setIsOpen] = useState(false);
+
+	const deleteSuccessToast = useToast();
+	const showDeleteSuccessToast = () =>
+		deleteSuccessToast({
+			title: "Account deletion successful",
+			description: "Why u have to delete :(",
+			status: "success",
+			duration: 3000,
+			isClosable: true,
+		});
+
+	const deleteFailureToast = useToast();
+	const showDeleteFailureToast = () =>
+		deleteFailureToast({
+			title: "Account deletetion unsuccessful",
+			description: "Something went wrong, please try again later",
+			status: "error",
+			duration: 3000,
+			isClosable: true,
+		});
 
 	const handleLogin = () => {
 		navigate("/login");
@@ -34,27 +66,37 @@ function NavBar() {
 		navigate("/");
 	};
 
+	const handleDeleteAccount = () => {
+		const resp = deleteUserAccount(user.uid);
+		resp.then((res) => {
+			if (res) {
+				onClose();
+				showDeleteSuccessToast();
+				clearUserData();
+				navigate("/");
+			} else {
+				showDeleteFailureToast();
+			}
+		})
+	}
+
 	const toggleOverlay = (e) => {
-		setIsOpen(e);
+		setIsOverlayOpen(e);
 	};
 
 	/* ======== Helper Functions ================================================================================*/
 	function getUsernameFromEmail(user) {
-		console.log(user);
 		if (!user || !user.email) {
 			return "NO USERNAME FOUND";
 		}
 
 		const email = user.email;
 		const username = email.split("@")[0];
-
-		console.log(username);
-
 		return username;
 	}
 	return (
 		<HStack w="100%" px="3%" py="1%" justifyContent="space-between">
-			<LeaveRoomOverlay isVisible={isOpen} toggleOverlay={toggleOverlay} />
+			<LeaveRoomOverlay isVisible={isOverlayOpen} toggleOverlay={toggleOverlay} />
 			<Heading color="#66ddaa">PeerPrep</Heading>
 			<HStack>
 				{location.pathname === "/matchroom" && (
@@ -87,9 +129,14 @@ function NavBar() {
 							<PopoverCloseButton />
 							<PopoverBody>
 								{idToken ? (
-									<Button colorScheme="teal" onClick={handleLogout}>
-										Logout
-									</Button>
+									<Stack>
+										<Button colorScheme="red" onClick={onOpen}>
+											Delete Account
+										</Button>
+										<Button colorScheme="teal" onClick={handleLogout}>
+											Logout
+										</Button>
+									</Stack>
 								) : (
 									<Button colorScheme="teal" onClick={handleLogin}>
 										Login
@@ -100,6 +147,34 @@ function NavBar() {
 					</Popover>
 				)
 				}
+				<>
+					<AlertDialog
+						isOpen={isOpen}
+						leastDestructiveRef={cancelRef}
+						onClose={onClose}
+					>
+						<AlertDialogOverlay>
+							<AlertDialogContent>
+								<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+									Delete Account
+								</AlertDialogHeader>
+
+								<AlertDialogBody>
+									Are you sure? You can't undo this action afterwards.
+								</AlertDialogBody>
+
+								<AlertDialogFooter>
+									<Button ref={cancelRef} onClick={onClose}>
+										Cancel
+									</Button>
+									<Button colorScheme='red' onClick={handleDeleteAccount} ml={3}>
+										Delete
+									</Button>
+								</AlertDialogFooter>
+							</AlertDialogContent>
+						</AlertDialogOverlay>
+					</AlertDialog>
+				</>
 			</HStack >
 		</HStack >
 	);
