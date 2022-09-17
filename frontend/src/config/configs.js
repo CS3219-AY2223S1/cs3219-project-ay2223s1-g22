e5594fcbaebf4457f0b5251411473b459ffbdc54
James Yeap <jamesyeap.ys@gmail.com>
2022-09-17 12:30:32 +0800

    Fix incorrect port to access User service in local setup

--- frontend/src/config/configs.js
+++ frontend/src/config/configs.js
@@ -1,6 +1,6 @@
 // URL for User Service endpoint
 const USER_SERVICE_DOMAIN =
-  "http://localhost:8080" || process.env.REACT_APP_USER_SERVICE_DOMAIN;
+  process.env.REACT_APP_USER_SERVICE_DOMAIN || "http://localhost:8081";
 const USER_SERVICE_PREFIX = "/api/firebaseauth";
 export const USER_SERVICE_URL = USER_SERVICE_DOMAIN + USER_SERVICE_PREFIX;
 
