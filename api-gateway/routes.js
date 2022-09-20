d99ef2cc2d278a8810d2d2804ebcc62519b018e0
James Yeap <jamesyeap.ys@gmail.com>
2022-09-20 18:06:28 +0800

    Add user service endpoint mappings

--- api-gateway/routes.js
+++ api-gateway/routes.js
@@ -1,17 +1,72 @@
 export const MATCHING_SERVICE_CLOUD_RUN_URL =
   "https://matching-service-pzsuad4zva-as.a.run.app:443";
 export const USER_SERVICE_CLOUD_RUN_URL =
-  "https://user-service-pzsuad4zva-as.a.run.app";
+  "https://user-service-pzsuad4zva-as.a.run.app" + "/api" + "/firebaseauth";
 
 export const HTTP_ROUTES = [
   {
-    auth: true,
-    url: "/premium",
+    auth: false,
+    url: "/signup",
     proxy: {
-      target: "https://www.google.com",
+      target: USER_SERVICE_CLOUD_RUN_URL + "/signup",
       changeOrigin: true,
       pathRewrite: {
-        [`^/premium`]: "",
+        [`^/signup`]: "",
+      },
+    },
+  },
+  {
+    auth: false,
+    url: "/login",
+    proxy: {
+      target: USER_SERVICE_CLOUD_RUN_URL + "/login",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/login`]: "",
+      },
+    },
+  },
+  {
+    auth: false,
+    url: "/refreshToken",
+    proxy: {
+      target: USER_SERVICE_CLOUD_RUN_URL + "/refreshToken",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/refreshToken`]: "",
+      },
+    },
+  },
+  {
+    auth: true,
+    url: "/getuser",
+    proxy: {
+      target: USER_SERVICE_CLOUD_RUN_URL + "/getuser",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/getuser`]: "",
+      },
+    },
+  },
+  {
+    auth: true,
+    url: "/logout",
+    proxy: {
+      target: USER_SERVICE_CLOUD_RUN_URL + "/revokeRefreshToken",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/logout`]: "",
+      },
+    },
+  },
+  {
+    auth: true,
+    url: "/deleteuser",
+    proxy: {
+      target: USER_SERVICE_CLOUD_RUN_URL + "/deleteuser",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/deleteuser`]: "",
       },
     },
   },
@@ -19,6 +74,7 @@ export const HTTP_ROUTES = [
 
 export const WEBSOCKET_ROUTES = [
   {
+    auth: true,
     url: "/get-match",
     proxy: {
       target: MATCHING_SERVICE_CLOUD_RUN_URL,
