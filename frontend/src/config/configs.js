04e81fb04c950cdc0599a8174eac2fd5dd957c40
James Yeap <jamesyeap.ys@gmail.com>
2022-10-07 14:35:03 +0800

    Update frontend to connect to collaboration service through API gateway

--- frontend/src/config/configs.js
+++ frontend/src/config/configs.js
@@ -1,5 +1,5 @@
 export const API_GATEWAY_URL =
   process.env.REACT_APP_API_GATEWAY_URL || "http://localhost:8080";
 
-export const COLLABORATION_SERVICE_URL =
-  process.env.REACT_APP_COLLABORATION_SERVICE_URL || "ws://localhost:8083";
+export const API_GATEWAY_WEBSOCKET_URL =
+  process.env.REACT_APP_API_GATEWAY_WEBSOCKET_URL || "ws://localhost:8080";
