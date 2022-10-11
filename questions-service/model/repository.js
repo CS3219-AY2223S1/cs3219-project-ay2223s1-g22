fbdf5a8f7d5f8eb0690a015198be3edb22b7dfed
James Yeap <jamesyeap.ys@gmail.com>
2022-10-11 16:18:30 +0800

    Update ports for question service

--- questions-service/model/repository.js
+++ questions-service/model/repository.js
@@ -1,22 +1,24 @@
-import QuestionModel from './question-model.js';
-import 'dotenv/config'
+import QuestionModel from "./question-model.js";
+import config from "../config/config.js";
 
 //Set up mongoose connection
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let mongoDB = process.env.DB_CLOUD_URI;
+let mongoDB = config.dbUrl;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 export async function createQuestion(params) {
-  return new QuestionModel(params)
+  return new QuestionModel(params);
 }
 
 export async function getQuestion(difficulty) {
   const questions = await QuestionModel.find({ difficulty: `${difficulty}` });
   console.log(questions);
-  const randomIndex = Math.floor(Math.random() * questions.length.toString().length * 10) % questions.length;
+  const randomIndex =
+    Math.floor(Math.random() * questions.length.toString().length * 10) %
+    questions.length;
   return questions[randomIndex];
 }
