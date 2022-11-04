78f9802dc1a98b949abe14e29837349510d57420
James Yeap <jamesyeap.ys@gmail.com>
2022-11-04 13:53:26 +0800

    Add light and dark mode to code editor

--- frontend/src/pages/matching-service/MatchRoomPage.js
+++ frontend/src/pages/matching-service/MatchRoomPage.js
@@ -1,4 +1,11 @@
-import { useToast, Grid, GridItem, Text, Heading } from "@chakra-ui/react";
+import {
+  useToast,
+  Grid,
+  GridItem,
+  Text,
+  Heading,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -10,72 +17,74 @@ import { SocketContext } from "./SocketContext";
 import UserContext from "../../UserContext";
 
 const MatchRoomPage = () => {
-	const state = useLocation().state;
-	const { idToken } = useContext(UserContext);
-	const { getSocket } = useContext(SocketContext);
-	const socketRef = useRef(getSocket(idToken));
+  const state = useLocation().state;
+  const { idToken } = useContext(UserContext);
+  const { getSocket } = useContext(SocketContext);
+  const socketRef = useRef(getSocket(idToken));
 
-	const reconnectSocket = (sock) => {
-		console.log("sending exit");
-		sock.emit("exit-room", state.roomNumber);
-		sock.disconnect();
-		sock.connect();
-	}
+  const reconnectSocket = (sock) => {
+    console.log("sending exit");
+    sock.emit("exit-room", state.roomNumber);
+    sock.disconnect();
+    sock.connect();
+  };
 
-	useEffect(() => {
-		const socket = socketRef.current;
-		socket.emit("enter-room", state.roomNumber);
-		return () => {
-			reconnectSocket(socket);
-		};
-	}, []);
+  useEffect(() => {
+    const socket = socketRef.current;
+    socket.emit("enter-room", state.roomNumber);
+    return () => {
+      reconnectSocket(socket);
+    };
+  }, []);
 
-	return (
-		<Grid
-			h="100vh"
-			templateRows="repeat(10, 1fr)"
-			templateColumns="repeat(6, 1fr)"
-			gap={4}
-		>
-			<GridItem rowSpan={1} colSpan={6}>
-				<NavBar />
-			</GridItem>
-			<GridItem
-				rowSpan={5}
-				colSpan={2}
-				marginLeft={2}
-				marginBottom={2}
-				border="2px"
-				borderRadius="10"
-				borderColor="blackAlpha.300"
-			>
-				<Question question={state.question} />
-			</GridItem>
-			<GridItem
-				rowSpan={4}
-				colSpan={2}
-				rowStart={7}
-				marginLeft={2}
-				marginBottom={2}
-				border="2px"
-				borderRadius="10"
-				borderColor="blackAlpha.300"
-			>
-				<Chat roomProps={state} />
-			</GridItem>
-			<GridItem
-				rowSpan={10}
-				colSpan={4}
-				marginBottom={2}
-				border="2px"
-				borderRadius="10"
-				borderColor="blackAlpha.300"
-				backgroundColor="linkedin.700"
-			>
-				<CodeEditor roomNumber={state.roomNumber} accessToken={idToken} />
-			</GridItem>
-		</Grid>
-	);
+  const editorBorderColor = useColorModeValue("gray.200", "gray.600");
+
+  return (
+    <Grid
+      h="100vh"
+      templateRows="repeat(10, 1fr)"
+      templateColumns="repeat(6, 1fr)"
+      gap={4}
+    >
+      <GridItem rowSpan={1} colSpan={6}>
+        <NavBar />
+      </GridItem>
+      <GridItem
+        rowSpan={5}
+        colSpan={2}
+        marginLeft={2}
+        marginBottom={2}
+        border="2px"
+        borderRadius="10"
+        borderColor="blackAlpha.300"
+      >
+        <Question question={state.question} />
+      </GridItem>
+      <GridItem
+        rowSpan={4}
+        colSpan={2}
+        rowStart={7}
+        marginLeft={2}
+        marginBottom={2}
+        border="2px"
+        borderRadius="10"
+        borderColor="blackAlpha.300"
+      >
+        <Chat roomProps={state} />
+      </GridItem>
+      <GridItem
+        rowSpan={10}
+        colSpan={4}
+        marginBottom={2}
+        border="2px"
+        borderRadius="10"
+        borderColor="blackAlpha.300"
+        backgroundColor={editorBorderColor}
+      >
+        <CodeEditor roomNumber={state.roomNumber} accessToken={idToken} />
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default MatchRoomPage;
